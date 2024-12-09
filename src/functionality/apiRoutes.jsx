3924f4de92c3351b46fc6cbf619652d2d75daed7
Maha Abo Assi <maha@onekw.com>
2024-12-09 15:23:23 +0400

    replace 5000 to 10000

--- src/functionality/apiRoutes.jsx
+++ src/functionality/apiRoutes.jsx
@@ -1,6 +1,6 @@
 // export const Host = "http://localhost:5000/api/"
 
-export const Host = "http://localhost:5000/api/"
+export const Host = "http://localhost:10000/api/"
 
 export const apiRouter = {
     auth : {
